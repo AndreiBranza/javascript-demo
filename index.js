68a1d03f6b0b7f74e206cdff4ce852083f947c28
Andrei Branza <andrei.branza@yahoo.com>
2024-04-17 17:56:48 +0300

    Created the job details component

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const submitHandler = event => {
     if(patternMatch) {
         errorTextEl.textContent = 'Your search may not contain Python';
         errorEl.classList.add('error--visible');
-        setTimeout(() => errorEl.classList.remove('error--visible'), 3500);
+        setTimeout(() => errorEl.classList.remove('error--visible'), 3501);
         return;  
     }
 
@@ -81,7 +81,7 @@ const submitHandler = event => {
                             <div class="job-item__extras">
                                 <p class="job-item__extra"><i class="fa-solid fa-clock job-item__extra-icon"></i>${element.duration}</p>
                                 <p class="job-item__extra"><i class="fa-solid fa-money-bill job-item__extra-icon"></i>${element.salary}</p>
-                                <p class="job-item__extra"><i class="fa-solid fa-location-dot job-item__extra-icon"></i> ${element}</p>
+                                <p class="job-item__extra"><i class="fa-solid fa-location-dot job-item__extra-icon"></i> ${element.location}</p>
                             </div>
                         </div>
                         <div class="job-item__right">
@@ -100,3 +100,104 @@ const submitHandler = event => {
 }
 
 searchFormEl.addEventListener('submit', submitHandler);
+
+// -- JOB LIST COMPONENT --
+const clickHandler = event => {
+    // prevent default behavior
+    event.preventDefault();
+
+    //get clicked job item element
+    const jobItemEl = event.target.closest('.job-item');
+
+    // remove the active class from previously active job item
+    document.querySelector('.job-item--active')?.classList.remove('job-item--active');
+    
+    //add active class
+    jobItemEl.classList.add('job-item--active');
+
+    //empty the job details section
+    jobDetailsContentEl.innerHTML = '';
+
+    //render spinner
+    spinnerJobDetailsEl.classList.add('spinner--visible');
+
+    //get the id from the href element
+    const id = jobItemEl.children[0].getAttribute('href');
+
+    //fetch job item data
+    fetch(`https://bytegrad.com/course-assets/js/2/api/jobs/${id}`)
+    .then(res => {
+        if(!res.ok) {
+            console.log('There has been an error');
+            return;
+        }
+        return res.json();
+    })
+    .then(data => {
+        console.log(data);
+        //extract job item
+        const { jobItem } = data;
+
+        //remove spinner
+        spinnerJobDetailsEl.classList.remove('spinner--visible');
+
+        //render the job details
+        jobDetailsContentEl.innerHTML = 
+            `<img src=${jobItem.coverImgURL} alt="#" class="job-details__cover-img">
+
+                <a class="apply-btn" href=${jobItem.companyURL} target="_blank">Apply <i class="fa-solid fa-square-arrow-up-right apply-btn__icon"></i></a>
+
+                <section class="job-info">
+                    <div class="job-info__left">
+                        <div class="job-info__badge">${jobItem.badgeLetters}</div>
+                        <div class="job-info__below-badge">
+                            <time class="job-info__time">${jobItem.daysAgo}</time>
+                            <button class="job-info__bookmark-btn">
+                                <i class="fa-solid fa-bookmark job-info__bookmark-icon"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="job-info__right">
+                        <h2 class="second-heading">${jobItem.title}</h2>
+                        <p class="job-info__company">${jobItem.company}</p>
+                        <p class="job-info__description">${jobItem.description}</p>
+                        <div class="job-info__extras">
+                            <p class="job-info__extra"><i class="fa-solid fa-clock job-info__extra-icon"></i>${jobItem.duration}</p>
+                            <p class="job-info__extra"><i class="fa-solid fa-money-bill job-info__extra-icon"></i>${jobItem.salary}</p>
+                            <p class="job-info__extra"><i class="fa-solid fa-location-dot job-info__extra-icon"></i>${jobItem.location}</p>
+                        </div>
+                    </div>
+                </section>
+
+                <div class="job-details__other">
+                    <section class="qualifications">
+                        <div class="qualifications__left">
+                            <h4 class="fourth-heading">Qualifications</h4>
+                            <p class="qualifications__sub-text">Other qualifications may apply</p>
+                        </div>
+                        <ul class="qualifications__list">
+                            ${jobItem.qualifications.map(
+                                qualificationText => `<li class='qualifications__item'>${qualificationText}</li>`).join(' ')}
+                        </ul>
+                    </section>
+
+                    <section class="reviews">
+                        <div class="reviews__left">
+                            <h4 class="fourth-heading">Company reviews</h4>
+                            <p class="reviews__sub-text">Recent things people are saying</p>
+                        </div>
+                        <ul class="reviews__list">
+                            ${jobItem.reviews.map(reviewText => `<li class='reviews__item'>${reviewText}</li>`).join(' ')}
+                        </ul>
+                    </section>
+                </div>
+
+                <footer class="job-details__footer">
+                    <p class="job-details__footer-text">If possible, please reference that you found the job on <span class="u-bold">rmtDev</span>, we would really appreciate it!</p>
+                </footer>`
+        
+    })
+    .catch(exception => console.log(exception));
+};
+
+jobListSearchEl.addEventListener('click', clickHandler);
